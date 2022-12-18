e645f67edb44738be5ce350a88a494c5f258a5b8
egeorge614 <brisketjustice@gmail.com>
2022-12-18 17:13:43 -0500

    removed comments

--- src/App.js
+++ src/App.js
@@ -8,21 +8,18 @@ import RecipeTodoList from "./RecipeTodoList"
 function App() {
   const [recipes, setRecipes] = useState(RecipeData); 
  
-  // Functions of State => Grabbing existing recipes and combining newRecipes
   const createNewRecipes = (newRecipes) => {
     setRecipes([
       ...recipes, newRecipes
     ])
   }
   
-  // Handler => Delete btn
   const deleteRecipe = (del) => {
      const filter = recipes.filter((recipe, index) => {return del !== index})
      setRecipes(filter)
     
   }
   
-  // Webpage return 
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
