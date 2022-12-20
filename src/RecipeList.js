86d96f684c467eb36c14b79f7a924f5c826c838f
egeorge614 <brisketjustice@gmail.com>
2022-12-20 12:02:56 -0500

    updated RecipeList with thead and tr

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -7,16 +7,16 @@ function RecipeList({ recipes, deleteRecipe }) {
   return (
     <div className="recipe-list">
       <table>
-        
-   
+        <thead>
+        <tr>
             <th>Name</th>
             <th>Cuisine</th>
             <th>Photo</th>
             <th>Ingredients</th>
             <th>Preparation</th>
             <th>Actions</th>
-        
-        
+            </tr>
+            </thead>
         <tbody>
     
           {recipes.map((recipe, index) => {
